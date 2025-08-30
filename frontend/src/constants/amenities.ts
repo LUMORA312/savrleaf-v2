dda1423730189641a7e9eb3b0a32a654a5b7105e
Ting Ting Chen Zeng <tin2294@gmail.com>
2025-08-30 11:59:03 +0200

    amenities options in constants

---
+++ frontend/src/constants/amenities.ts
@@ -0,0 +1,14 @@
+export const amenitiesOptions = [
+  'Parking',
+  'Wi-Fi',
+  'Wheelchair Accessible',
+  'Outdoor Seating',
+  'Delivery',
+  'Curbside Pickup',
+  'Lounge Area',
+  'Pet Friendly',
+  'Cash Accepted',
+  'ATM On-Site',
+  'Private Consultation',
+  'Educational Materials',
+];
